723b8c2a9792c7c94c12fac172a3b4d056dbe3ee
JasonWues <panruzun@gmail.com>
2021-12-09 16:18:16 +0800

    base work

---
+++ Entity/BaseId.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entity
+{
+    public class BaseId
+    {
+        [Key]
+        [Column(TypeName = "varchar(36)")]
+        public string Id { get; set; }
+    }
+}
