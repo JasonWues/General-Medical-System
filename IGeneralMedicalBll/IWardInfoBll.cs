15a9f9f60433cc053049ccab1ea9ce839ec46966
takt-Destiny <876788400@qq.com>
2021-12-10 15:17:52 +0800

    WardInfoBll和IBll

---
+++ IGeneralMedicalBll/IWardInfoBll.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGeneralMedicalBll
+{
+    /// <summary>
+    /// 病房业务访问层接口
+    /// </summary>
+    public interface IWardInfoBll : IBaseBll<WardInfo>
+    {
+
+    }
+}
