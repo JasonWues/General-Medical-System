32f95d012d6e140df91361e92237df9f9df2e87d
takt-Destiny <876788400@qq.com>
2021-12-10 15:28:45 +0800

    RoleInfoBll和IBll

---
+++ GeneralMedicalBll/RoleInfoBll.cs
@@ -0,0 +1,22 @@
+﻿using Entity;
+using IGeneralMedicalBll;
+using IGeneralMedicalDal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralMedicalBll
+{
+    /// <summary>
+    /// 角色业务访问层
+    /// </summary>
+    public class RoleInfoBll : BaseBll<RoleInfo>,IRoleInfoBll
+    {
+        public RoleInfoBll(IRoleInfoDal roleInfoDal)
+        {
+           _iBaseDal = roleInfoDal; 
+        }
+    }
+}
