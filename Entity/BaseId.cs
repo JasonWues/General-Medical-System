8c6e52d9b5645234252e30621948399bc8458598
TangHaoOOO <1547925015@qq.com>
2021-12-10 15:17:24 +0800

    测试

--- Entity/BaseId.cs
+++ Entity/BaseId.cs
@@ -3,8 +3,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entity
 {
+    /// <summary>
+    /// 基础父类实体
+    /// </summary>
     public class BaseId
     {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        
         [Key]
         [Column(TypeName = "varchar(36)")]
         public string Id { get; set; }
