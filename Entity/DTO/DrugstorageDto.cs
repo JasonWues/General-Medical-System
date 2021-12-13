853f5d670dfbc8b1583e23f0712f25b1639f4e6a
SuSu <1441589107@qq.com>
2021-12-13 21:47:53 +0800

    Drugstorage添加控制器

---
+++ Entity/DTO/DrugstorageDto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.DTO
+{
+    public record DrugstorageDto
+    {
+        public string Id { get; set; }    
+        /// <summary>
+        /// 药品名称
+        /// </summary>
+
+        public string DrugTitle { get; set; }
+
+        /// <summary>
+        /// 单位
+        /// </summary>
+
+        public string Unit { get; set; }
+
+        /// <summary>
+        /// 库存
+        /// </summary>
+        public int Stock { get; set; }
+
+        /// <summary>
+        /// 警戒数字
+        /// </summary>
+        public int Warningcount { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// 价格
+        /// </summary>
+
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 生产厂家
+        /// </summary>
+
+        public string ManufacturerName { get; set; }
+
+        /// <summary>
+        /// 软删除
+        /// </summary>
+        public bool IsDelete { get; set; }
+
+        /// <summary>
+        /// 删除时间
+        /// </summary>
+        public string Deletetime { get; set; }
+    }
+}
---
+++ General Medical System Webapi/Controllers/DrugstorageController.cs
@@ -0,0 +1,49 @@
+﻿using Entity;
+using Entity.DTO;
+using IGeneralMedicalBll;
+using MapsterMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Utility;
+
+namespace General_Medical_System_Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrugstorageController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IDrugstorageBll _dirugstorageBll;
+        public DrugstorageController(IMapper mapper, IDrugstorageBll dirugstorageBll)
+        {
+            _mapper = mapper;
+            _dirugstorageBll = dirugstorageBll;
+        }
+        /// <summary>
+        /// 查询全部
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ApiResult>Qurey()
+        {
+            var dirugstorages= await _dirugstorageBll.GetAll().ToListAsync();
+            var drugstorageDtos= _mapper.Map<List<DrugstorageDto>>(dirugstorages);
+            if(drugstorageDtos.Count!=0)return ApiResultHelp<List<DrugstorageDto>>.SuccessResult(drugstorageDtos);
+            return ApiResultHelp.ErrorResult(404, "无数据");
+        }
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="drugstorage"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ApiResult>Add(Drugstorage drugstorage)
+        {
+            if (await _dirugstorageBll.AddAsync(drugstorage)) return ApiResultHelp.SuccessResult();
+            return ApiResultHelp.ErrorResult(405, "添加失败");
+        }
+
+
+    }
+}
