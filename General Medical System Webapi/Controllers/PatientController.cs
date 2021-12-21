700a4a43958d4dcd0dcfbc77a42516a160645818
TangHaoOOO <1547925015@qq.com>
2021-12-21 09:50:09 +0800

    将wardInfoOption改为wardOption

--- General Medical System Webapi/Controllers/PatientController.cs
+++ General Medical System Webapi/Controllers/PatientController.cs
@@ -20,7 +20,7 @@ namespace General_Medical_System_Webapi.Controllers
         private readonly IPatientInfoBll _patientInfoBll;
         private readonly IWardInfoBll _wardInfoBll;
 
-        public PatientController(IMapper mapper, IPatientInfoBll patientInfoBll,IWardInfoBll wardInfoBll)
+        public PatientController(IMapper mapper, IPatientInfoBll patientInfoBll, IWardInfoBll wardInfoBll)
         {
             _mapper = mapper;
             _patientInfoBll = patientInfoBll;
@@ -33,11 +33,11 @@ namespace General_Medical_System_Webapi.Controllers
         /// <returns></returns>
         /// Get api/Patient
         [HttpGet]
-        public async Task<ApiResult> Query(int page, int limit,string? patientName, string? phoneNum)
+        public async Task<ApiResult> Query(int page, int limit, string? patientName, string? phoneNum)
         {
-            var (patients,count) = await _patientInfoBll.Query(page, limit, patientName, phoneNum);
+            var (patients, count) = await _patientInfoBll.Query(page, limit, patientName, phoneNum);
             //var patientDtos = _mapper.Map<List<PatientDto>>(patients);
-            if (patients.Count != 0) return ApiResultHelp<List<Patient_Ward>>.SuccessResult(patients,count);
+            if (patients.Count != 0) return ApiResultHelp<List<Patient_Ward>>.SuccessResult(patients, count);
             return ApiResultHelp.ErrorResult(404, "无数据");
         }
 
@@ -118,5 +118,22 @@ namespace General_Medical_System_Webapi.Controllers
             if (await _patientInfoBll.DeleteAsync(id)) return ApiResultHelp.SuccessResult();
             return ApiResultHelp.ErrorResult(405, "删除失败");
         }
+
+
+        [HttpGet("wardOption")]
+        public async Task<List<WardInfo>>GetSelectOption()
+        {
+            var option = await _wardInfoBll.GetEntities.Select(x => new WardInfo
+            { 
+                Id = x.Id, 
+                WardTitle = x.WardTitle,
+            }).ToListAsync();
+
+            if (option.Count != 0) return option;
+            return new List<WardInfo>();
+        }
+
+
+
     }
 }
